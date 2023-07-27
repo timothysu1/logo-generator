d5fd6544b205954c556491d06257e55dbfd3ee65
Timothy <timsu100@gmail.com>
2023-07-27 01:27:04 -0700

    add input to svg maker

--- test/square.test.js
+++ test/square.test.js
@@ -4,6 +4,6 @@ describe('Square', () => {
   it('should render a green square', () => {
     const shape = new Square();
     shape.setColor('green');
-    expect(shape.render()).toEqual('<rect width="200" height="200" fill="green" />')
+    expect(shape.render()).toEqual('<rect x="75" y="25"width="150" height="150" fill="green" />')
   })
 })
--- test/triangle.test.js
+++ test/triangle.test.js
@@ -4,6 +4,6 @@ describe('Triangle', () => {
   it('should render a blue triangle', () => {
     const shape = new Triangle();
     shape.setColor('blue');
-    expect(shape.render()).toEqual('<polygon points="115, 0 230, 200 0, 200" fill="blue" />')
+    expect(shape.render()).toEqual('<polygon points="150, 18 244, 182 56, 182" fill="blue" />')
   })
 })
